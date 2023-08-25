56d2e10882f2fe17bf99d5ca4642263a90df2882
Polla <polla.fattah@gmail.com>
2023-08-26 00:54:36 +0300

    2- Even Fibonacci Numbers

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        long count1 = 0;
-        long count2 = 0;
-        long startTime = System.nanoTime();
+        long firstNumber = 1;
+        long secondNumber = 1;
+        long newNumber;
+        long evenTotal = 0;
+        while(secondNumber < 4_000_000){
 
-        for(int i = 1; i < 1000000; i++){
-            if( i % 3 == 0 || i % 5 == 0)
-                count1 += i;
+            if(secondNumber % 2 == 0)
+                evenTotal += secondNumber;
+
+            newNumber = firstNumber + secondNumber;
+            firstNumber = secondNumber;
+            secondNumber = newNumber;
         }
-
-        long midTime = System.nanoTime();
-
-        midTime -= startTime;
-        startTime = System.nanoTime();
-        for(int i = 3; i < 1000000; i+=3){
-            count2 += i;
-        }
-        for(int i = 5; i < 1000000; i+=5){
-            if(i % 3 != 0)
-                count2 += i;
-        }
-
-        long endTime = System.nanoTime();
-
-        // get the difference between the two nano time valuess
-        long timeElapsed = endTime - startTime;
-        System.out.println(count1);
-        System.out.println(count2);
-
-        System.out.println(midTime/    1000000.0);
-        System.out.println(timeElapsed/1000000.0);
+        System.out.println(evenTotal);
 
     }
 }
