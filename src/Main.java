d5268120aad9a4c63bec189932b2e756d033a114
Polla <polla.fattah@gmail.com>
2023-08-27 01:45:57 +0300

    4- Largest Palindrome Product

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,27 @@ import java.util.TreeSet;
 
 // from rosetta stone
 public class Main {
-
-
     public static void main(String[] str) {
-        long n = 600851475143L;
-
-        TreeSet<Long> factors = new TreeSet<>();
-
-        Long current_test = 2L;
-        while (n != 1L) {
-            while (n % current_test == 0L) {
-                factors.add(current_test);
-                n = n / current_test;
+        int pal = 0;
+        for (int i = 999; i >= 100; i--) {
+            for (int j = 999; j >= i; j--) {
+                int t = i * j;
+                if (isPalindrome("" + t)) {
+                    if (pal < t)
+                        pal = t;
+                }
             }
-            current_test++;
-
         }
-        System.out.println(factors.last());
+        System.out.println(pal);
     }
 
-
+    public static boolean isPalindrome(String p) {
+        int length = p.length() - 1;
+        int loop = length / 2;
+        for (int i = 0; i <= loop; i++) {
+            if (p.charAt(i) != p.charAt(length - i))
+                return false;
+        }
+        return true;
+    }
 }
