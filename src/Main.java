256d4f5bed9716a5ad164e553dfec3ea17485aeb
Polla <polla.fattah@gmail.com>
2023-08-27 00:44:33 +0300

    3- Largest Prime Factor

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,25 @@
+import java.util.TreeSet;
+
+// from rosetta stone
 public class Main {
-    public static void main(String[] args) {
-        long firstNumber = 1;
-        long secondNumber = 1;
-        long newNumber;
-        long evenTotal = 0;
-        while(secondNumber < 4_000_000){
 
-            if(secondNumber % 2 == 0)
-                evenTotal += secondNumber;
 
-            newNumber = firstNumber + secondNumber;
-            firstNumber = secondNumber;
-            secondNumber = newNumber;
+    public static void main(String[] str) {
+        long n = 600851475143L;
+
+        TreeSet<Long> factors = new TreeSet<>();
+
+        Long current_test = 2L;
+        while (n != 1L) {
+            while (n % current_test == 0L) {
+                factors.add(current_test);
+                n = n / current_test;
+            }
+            current_test++;
+
         }
-        System.out.println(evenTotal);
-
+        System.out.println(factors.last());
     }
+
+
 }
