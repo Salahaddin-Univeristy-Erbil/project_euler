42a15f1f6eb4fd6be81b3c4c90fa12fb46167326
Polla <polla.fattah@gmail.com>
2023-08-28 19:52:10 +0300

    10- Summation of Primes

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,27 @@
 import java.util.LinkedList;
-import java.math.*;
 
-// from rosetta stone
 public class Main {
+    public static LinkedList<Long> primes = new LinkedList<>();
     public static void main(String[] str) {
-        for(long a = 1; a < 1000; a++) {
-            for (long b = 1; b < a; b++) {
-                long c = 1000 - (a + b);
-                if (Math.pow(a, 2) + Math.pow(b, 2) == Math.pow(c, 2)) {
-                    System.out.println(a * b * c);
-                    System.out.println("a=" + a + ", b=" + b + ", c=" + c);
-                    System.exit(0);
-                }
-            }
-        }
 
+        primes.add(2L);
+        long next = 3L;
+        while(next < 2_000_000){
+            if(isPrime(next)) {
+                primes.add(next);
+            }
+            next += 2;
+        }
+        long sum = 0;
+        for(long i: primes)
+            sum += i;
+        System.out.println(sum);
+    }
+    public static boolean isPrime(Long p){
+        for(Long l : primes)
+            if(p % l == 0)
+                return false;
+        return true;
     }
 
 }
